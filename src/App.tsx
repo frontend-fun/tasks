4688e92d5f3b054169797bdc8176554da088e31c
JaredThacker <pricesmayvary302@gmail.com>
2024-08-24 23:57:51 -0400

    solved-html-css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -11,6 +12,52 @@ function App(): React.JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            This is <span style={{ color: "green" }}> green text</span>
+            <div style={{ border: "1px solid blue", padding: "4px" }}>
+                {" "}
+                this will be surrounded by blue
+            </div>
+            <h1>header 1?</h1>
+            <ul>
+                <li>item 1</li>
+                <li>item 2</li>
+                <li>item 3</li>
+            </ul>
+            <img
+                src="https://play-lh.googleusercontent.com/NWemNZl1lOc8KQBgIp1hd8io4VMz37uoak0KvtbX_RT6DzRPPdJ5L-sGFJoCr87gTio=w240-h480-rw"
+                alt="stock image"
+            />
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
+            <Container>
+                <Row>
+                    <Col>
+                        First Column
+                        <div
+                            style={{
+                                width: "700px",
+                                height: "70px",
+                                backgroundColor: "red",
+                            }}
+                        ></div>
+                    </Col>
+                    <Col>
+                        second column
+                        <div
+                            style={{
+                                width: "700px",
+                                height: "70px",
+                                backgroundColor: "red",
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
