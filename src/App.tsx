f59678929040d5707bc0e16f6f6485f3b304b89b
ethfass <ethfass@udel.edu>
2024-08-30 11:13:33 -0400

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>Ethan Fassnacht - for credit</p>
+            <p>Ethan Fassnacht - name for credit</p>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
