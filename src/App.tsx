7383884fed7fe5327d71c00a072b4ca989e49a81
ZhihuaSun1 <sunzhi@udel.edu>
2024-08-30 11:53:14 -0400

    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import "./App.css";
-import { Button } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
         <>
             <h1>
-                <span style={{ color: "red", border: '1px solid red', padding: '4px'}}>MY FIRST HOMEWORK </span>
+                <span
+                    style={{
+                        color: "red",
+                        border: "1px solid red",
+                        padding: "4px",
+                    }}
+                >
+                    MY FIRST HOMEWORK{" "}
+                </span>
                 <ol>
                     <li>get A</li>
                     <li>Become a Software Engineer</li>
@@ -15,8 +23,17 @@ function App(): React.JSX.Element {
                 <img src="public\logo192.png" alt="A picture" />
                 <div className="App">
                     <header className="App-header">
+                        <Container>
+                             <Row>
+                                <Col>
                         UD CISC275 with React Hooks and TypeScript ZhihuaSun HHH
                         Hello World
+                        </Col>
+                        <Col>
+                        Second prat
+                        </Col>
+                    </Row>
+                    </Container>
                     </header>
                     <p>
                         Edit <code>src/App.tsx</code> and save. This page will
