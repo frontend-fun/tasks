3f072fb6f4ba17add94bb8a27027c3f683ace06c
BrishnaN <brishna@udel.edu>
2024-09-01 16:55:20 -0400

    Added Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,10 @@ function App(): React.JSX.Element {
                 <h1>My name is Brishna</h1>
                 UD CISC275 with React Hooks and TypeScript
             </header>
+            <p>Hello World</p>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
+                automatically reload. Hello World
             </p>
         </div>
     );
