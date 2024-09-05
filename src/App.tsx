71afa6a0c3849c83c07722388b053643ccae1c82
Shukria <shukriam@udel.edu>
2024-09-05 14:00:45 -0400

    Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,8 @@ function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                "Hello World"
                 <h1>Shukria Muhammadi</h1>
-                UD CISC275 with React Hooks and TypeScript
+                Hello World UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
