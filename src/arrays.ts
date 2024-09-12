b1d97f8748beef0008d92f0bf74f2e73a94f85b1
Ethan <eseifer@udel.edu>
2024-09-12 13:20:37 -0400

    Completed tripleNumbers function.

--- src/arrays.ts
+++ src/arrays.ts
@@ -23,7 +23,8 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    const tripled = numbers.map((numba: number): number => numba * 3);
+    return tripled;
 }
 
 /**
