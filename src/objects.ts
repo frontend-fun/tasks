5be03d2f8a7aa47c2ac418edc9aaa30cb8c18ec3
Ethan <eseifer@udel.edu>
2024-09-17 00:25:34 -0400

    Completed isValid function. Had me fed up for a lil.

--- src/objects.ts
+++ src/objects.ts
@@ -48,7 +48,19 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    return false;
+    if (question.type === "short_answer_question") return true;
+    else {
+        /*const trimmedAnswer = answer.trim();
+        const finalAnswer = trimmedAnswer.toLowerCase();
+        const ops = question.options.map((option: string): string =>
+            option.trim().toLowerCase(),
+        );
+        /*const answers = ops.filter(
+            (option: string): boolean => option === finalAnswer,
+        );
+        */
+        return question.options.includes(answer);
+    }
 }
 
 /**
