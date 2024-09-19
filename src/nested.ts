aacdd924dd55094991eb24f29a138935507cd15a
Ethan <eseifer@udel.edu>
2024-09-19 00:37:43 -0400

    Completed removeQuestion function.

--- src/nested.ts
+++ src/nested.ts
@@ -47,7 +47,10 @@ export function findQuestion(
  * with the given `id`.
  */
 export function removeQuestion(questions: Question[], id: number): Question[] {
-    return [];
+    const removed = questions.filter(
+        (question: Question): boolean => question.id !== id,
+    );
+    return removed;
 }
 
 /***
