f6c0a387eea3ff43128e556d501bb6097454c538
Ethan <eseifer@udel.edu>
2024-09-19 00:41:36 -0400

    Completed sumPoints function.

--- src/nested.ts
+++ src/nested.ts
@@ -66,7 +66,12 @@ export function getNames(questions: Question[]): string[] {
  * Consumes an array of questions and returns the sum total of all their points added together.
  */
 export function sumPoints(questions: Question[]): number {
-    return 0;
+    const points = questions.reduce(
+        (currentTotal: number, question: Question): number =>
+            currentTotal + question.points,
+        0,
+    );
+    return points;
 }
 
 /***
