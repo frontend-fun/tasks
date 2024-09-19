a244ed126d3a5dc34a65d37668dc1a221a0d2ec3
Ethan Fassnacht <ethfass74@gmail.com>
2024-09-19 12:50:48 -0400

    pls

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App(): React.JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
-            <p>Ethan Fassnacht - CISC275</p>
+            <p>CISC275</p>
         </div>
     );
 }
