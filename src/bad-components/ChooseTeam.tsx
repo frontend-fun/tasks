c58442ede0f0eb8a250c108238a6ae6333b76180
matis <matiass@udel.edu>
2024-09-25 13:20:47 -0400

    solved bad components

--- src/bad-components/ChooseTeam.tsx
+++ src/bad-components/ChooseTeam.tsx
@@ -1,3 +1,4 @@
+
 import React, { useState } from "react";
 import { Button, Row, Col } from "react-bootstrap";
 
@@ -14,18 +15,18 @@ export function ChooseTeam(): React.JSX.Element {
     const [allOptions, setAllOptions] = useState<string[]>(PEOPLE);
     const [team, setTeam] = useState<string[]>([]);
 
-    function chooseMember() {
-        /*
+    function chooseMember(newMember:string) {
+        
         if (!team.includes(newMember)) {
-            team.push(newMember);
+            setTeam([...team,newMember])
+            setAllOptions(allOptions.filter((option:string):boolean => option !=newMember));
         }
-        */
+        
     }
 
     function clearTeam() {
-        /*
-        team = [];
-        */
+        setTeam([]);
+        setAllOptions(PEOPLE);
     }
 
     return (
@@ -36,7 +37,7 @@ export function ChooseTeam(): React.JSX.Element {
                     {allOptions.map((option: string) => (
                         <div key={option} style={{ marginBottom: "4px" }}>
                             Add{" "}
-                            <Button onClick={chooseMember} size="sm">
+                            <Button onClick={() => {chooseMember(option)}} size="sm">
                                 {option}
                             </Button>
                         </div>
--- src/bad-components/ShoveBox.tsx
+++ src/bad-components/ShoveBox.tsx
@@ -1,56 +1,45 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
-function ShoveBoxButton({
-    position,
-    setPosition,
-}: {
-    position: number;
-    setPosition: (newPosition: number) => void;
-}) {
-    return (
-        <Button
-            onClick={() => {
-                setPosition(4 + position);
-            }}
-        >
-            Shove the Box
-        </Button>
-    );
-}
 
-function MoveableBox(): React.JSX.Element {
-    const [position, setPosition] = useState<number>(10);
-    return (
-        <div
-            data-testid="moveable-box"
-            style={{
-                width: "50px",
-                height: "50px",
-                backgroundColor: "lightblue",
-                border: "1px solid blue",
-                display: "inline-block",
-                verticalAlign: "bottom",
-                marginLeft: position + "px",
-            }}
-        ></div>
-    );
-}
 
 export function ShoveBox(): React.JSX.Element {
+    const [position, setPosition] = useState<number>(10);
+    
+
+    function MoveableBox(): React.JSX.Element {
+        
+        return (
+            <div
+                data-testid="moveable-box"
+                style={{
+                    width: "50px",
+                    height: "50px",
+                    backgroundColor: "lightblue",
+                    border: "1px solid blue",
+                    display: "inline-block",
+                    verticalAlign: "bottom",
+                    marginLeft: position + "px",
+                }}
+            ></div>
+        );
+    }
     const box = MoveableBox();
 
     return (
         <div>
             <h3>Shove Box</h3>
-            {/* <span>The box is at: {box.position}</span>
+            <span>The box is at: {position}</span>
             <div>
-                <ShoveBoxButton
-                    position={box.position}
-                    setPosition={box.setPosition}
-                ></ShoveBoxButton>
+                <Button
+                onClick={() => {
+                    setPosition(4 + position);
+                }}
+            >
+                Shove the Box
+            </Button>
                 {box}
-            </div> */}
+            </div>
         </div>
     );
 }
