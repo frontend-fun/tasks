ae988a009ac61a7c21df13fae55f7df206d8ac2b
Ethan Fassnacht <ethfass74@gmail.com>
2024-09-25 00:14:19 -0400

    task 10

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,36 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer,
 }: {
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [answer, setAnswer] = useState<string>("");
+    const [show, setShow] = useState<string>("❌");
+
+    function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        setAnswer(event.target.value);
+        if (event.target.value === expectedAnswer) {
+            setShow("✔️");
+        } else {
+            setShow("❌");
+        }
+    }
+
     return (
         <div>
             <h3>Check Answer</h3>
+            <Form.Group controlId="basicQuestion">
+                <Form.Label>Who won the 2023 NBA MVP?</Form.Label>
+                <Form.Control
+                    type="text"
+                    placeholder="Enter answer"
+                    value={answer}
+                    onChange={updateAnswer}
+                />
+            </Form.Group>
+            <div>{show}</div>
         </div>
     );
 }
--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,31 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [userChoice, setUserChoice] = useState<string>(options[0]);
+    const [show, setShow] = useState<string>("❌");
+
+    function updateUserChoice(event: React.ChangeEvent<HTMLSelectElement>) {
+        setUserChoice(event.target.value);
+        if (event.target.value === expectedAnswer) {
+            setShow("✔️");
+        } else {
+            setShow("❌");
+        }
+    }
+
     return (
         <div>
-            <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="multi">
+                <Form.Label>Select an answer</Form.Label>
+                <Form.Select value={userChoice} onChange={updateUserChoice}>
+                    {options.map((element) => (
+                        <option key={element} value={element}>
+                            {element}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            <div>{show}</div>
         </div>
     );
 }
