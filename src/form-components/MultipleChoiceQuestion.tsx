661313153b600c7bbe9162bf671f3058cefed215
Matthew <themattg@udel.edu>
2024-09-26 23:03:33 -0400

    MultipleChoiceQuestions finished

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,29 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [answer, setAnswer] = useState<string>(options[0]);
+    const [correctness, switchCorrectness] = useState<string>("❌");
+    function changeAnswer(event: React.ChangeEvent<HTMLSelectElement>) {
+        setAnswer(event.target.value);
+        if (event.target.value == expectedAnswer) {
+            switchCorrectness("✔️");
+        } else {
+            switchCorrectness("❌");
+        }
+    }
     return (
         <div>
-            <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="favoriteColors">
+                <Form.Label>What is your favorite color?</Form.Label>
+                <Form.Select value={answer} onChange={changeAnswer}>
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            {correctness}
         </div>
     );
 }
