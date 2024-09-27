2b31e21323112a684381ca154dfdfbcee1886a41
Jared <jmtigers@udel.edu>
2024-09-27 16:56:37 -0400

    Worked on ChangedColor.tsx, haven't implemented backgroundColor

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,45 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function ChangeColor(): React.JSX.Element {
+    const [color, setColor] = useState<string>("red");
+    const colorArr = [
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "cyan",
+        "blue",
+        "purple",
+        "magenta",
+        "black",
+        "white",
+    ];
+
+    function updateColor(event: React.ChangeEvent<HTMLInputElement>) {
+        setColor(event.target.value);
+    }
+
     return (
         <div>
             <h3>Change Color</h3>
+            {colorArr.map((c: string) => (
+                <Form.Check
+                    inline
+                    type="radio"
+                    name="colors"
+                    onChange={updateColor}
+                    id="color-check"
+                    label={c}
+                    key={c}
+                    value={c}
+                    checked={color === c}
+                />
+            ))}
+            <br></br>
+            <span data-testid="colored-box">You have chosen</span>
+            <span>{color}</span>
+            <span>.</span>
         </div>
     );
 }
