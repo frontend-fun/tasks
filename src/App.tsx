3f61c49a5d113ddb01a85072a3f4d9ceb267dc0c
Ben <benmonti@udel.edu>
2025-08-29 11:07:12 -0400

    Added name to app

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ function App(): React.JSX.Element {
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
+                <br />
+                <br />
+                Ben Montimurro
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
