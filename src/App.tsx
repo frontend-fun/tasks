7a3359c922c78c770670892969be4ee4428834fb
evanwalz <evanwalz@udel.edu>
2025-08-29 11:25:06 -0400

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,7 @@ function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                Evan Walz - 
-                UD CISC275 with React Hooks and TypeScript
+                Evan Walz - UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
