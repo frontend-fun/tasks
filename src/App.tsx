23ac51b51130c04fd356f29199e5a85dcd1fe4f0
Angel Jose <Angel@wifi-roaming-128-4-101-12.wifi.udel.edu>
2025-09-03 15:10:02 -0400

    Add Hello World to App

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,9 @@ function App(): React.JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <p>
+                Hello World
+            </p>
         </div>
     );
 }
