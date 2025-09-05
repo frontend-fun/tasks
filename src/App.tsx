82082ed1c6339435f0d03abd2bf44f958e5addf5
Courtney Chioma <cuchioma@udel.edu>
2025-09-05 14:17:00 -0400

    Solved Basic HTML and CSS Task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import eagles from "./images/eagles.png";
+import { Container, Row, Col } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -7,11 +9,66 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript - Courtney Chioma
             </header>
+
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
-            <p> Hello World</p>
+
+            <p>Hello World</p>
+            <h1>Task 3</h1>
+
+            <img
+                src={eagles}
+                alt="A picture of eagles logo. Go Birds!"
+                width={240}
+            />
+
+            <ul>
+                <li>First Thing</li>
+                <li>Second Thing</li>
+                <li>Third Thing</li>
+            </ul>
+
+            <p>
+                This is <span style={{ color: "red" }}>colored text</span>.
+            </p>
+            <div style={{ border: "1px solid blue", padding: "4px" }}>
+                this will be surrounded by a border and padding.
+            </div>
+
+            <button
+                className="btn btn-primary"
+                type="button"
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </button>
+
+            <Container className="mt-3">
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                width: "100%",
+                                height: "120px",
+                                backgroundColor: "red",
+                            }}
+                        />
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                width: "100%",
+                                height: "120px",
+                                backgroundColor: "red",
+                            }}
+                        />
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
