2c8c10a5a3e917d5d7f2f041705187a8218443a6
Samita Bomasamudram <samiboma@udel.edu>
2025-09-04 22:40:06 -0400

    Task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import paris from "./paris.jpg";
+import { Button, Container, Row, Col } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -13,6 +15,45 @@ function App(): React.JSX.Element {
             </p>
             <p>Samita Bomasamudram</p>
             <p> Hello World!!</p>
+            <h1>This is a header</h1>
+            <img src={paris} alt="Art of Paris" />
+            <ul>
+                <li>task 1</li>
+                <li>task 2</li>
+                <li>task 3</li>
+            </ul>
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
+
+            <Container>
+                <Row>
+                    <Col>
+                        First column
+                        <div
+                            style={{
+                                width: "10%",
+                                height: 20,
+                                backgroundColor: "red",
+                            }}
+                        />
+                    </Col>
+                    <Col>
+                        Second column
+                        <div
+                            style={{
+                                width: "10%",
+                                height: 20,
+                                backgroundColor: "red",
+                            }}
+                        />
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
