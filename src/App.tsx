b981e45390f088e712152652bde6b467d8afca30
Ben Montimurro <benmonti@udel.edu>
2025-09-05 14:38:15 -0400

    Added header, image, list, working button, and two red columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import doodlebob from "./images/doodlebob.png";
+import { Button, Container, Row, Col } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -13,6 +15,45 @@ function App(): React.JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World
             </p>
+            <br />
+            <h1>This is doodlebob</h1>
+            <img src={doodlebob} alt="doodlebob" />
+            <ul className="App-list">
+                <li>Doodlebob is dangerous</li>
+                <li>Doodlebob was made by spongebob</li>
+                <li>Doodlebob uses a pencil to make things come to life</li>
+            </ul>
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
+            <div>
+                <Container>
+                    <Row>
+                        <Col
+                            style={{
+                                height: 300,
+                                width: 50,
+                                backgroundColor: "red",
+                            }}
+                        >
+                            This is the first column
+                        </Col>
+                        <Col
+                            style={{
+                                height: 300,
+                                width: 50,
+                                backgroundColor: "red",
+                            }}
+                        >
+                            This is the second column
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         </div>
     );
 }
